2ce58805d7ef5528cf5aaaa3fce9c0c19171ca1b
nikola-tomik <nikola.tomik12@gmail.com>
2020-12-20 17:09:09 +0100

    [IA] feature: added front and front logic for this sprint
    Co-authored-by: mark-ristic <risticmark1337@gmail.com>
    Co-authored-by: dparipovic <danilo.paripovic@yahoo.com>
    Co-authored-by: aleksic-nikola <nikola.aleksi@gmail.com>

--- IntegrationAdapters/IntegrationAdapters/wwwroot/js/medOrder.js
+++ IntegrationAdapters/IntegrationAdapters/wwwroot/js/medOrder.js
@@ -38,21 +38,20 @@ function getSpec() {
                 alert("There is no available specification for given Medicine!");
 
                 $.ajax({
-                    type: 'GET',
-                    crossDomain: true,
-                    url: 'http://localhost:8080/medspec/' + name,
-                    
-                    success: function() {
-                        $.ajax({
-                            type: 'GET',
-                            url: 'api/report/ftp/' + name,
-                            
-                            success: function() {
-                                getSpec();
-                            }
-                        })
+                    type: "GET",
+                    url: "/api/http/env",
+            
+                    success : function(data) {
+                        console.log(data);
+            
+                        if(String(data) === "Development"){                            
+                            getSpecSftp(name);
+                        } else {
+                            getSpecHttp(name);
+                        }
                     }
                 })
+    
             } else {
                 alert("Opened the specification file for you!");
             }
@@ -61,17 +60,100 @@ function getSpec() {
 }
 
 function getAvailability() {
+
+    $.ajax({
+        type: "GET",
+        url: "/api/http/env",
+
+        success : function(data) {
+            console.log(data);
+
+            if(String(data) === "Development"){
+                checkGRPC();
+            } else {
+                checkHTTP();
+            }
+        }
+    })
+
+}
+
+function sendPrescription(){
+    var patient = $("#patientName").val();
     var name = $("#medID").val();
     var amount = $("#amount").val();
+    var pharmacy = "Benu";
 
     $.ajax({
         type: 'POST',
-        crossDomain: true,
-        url: 'http://localhost:8080/med/availability',
+        url: 'api/prescription/generate',
 
         data : JSON.stringify({
-            name : name,
-            amount : amount
+            Patient : patient, 
+            Medicine : name,
+            Amount : parseInt(amount),
+            Pharmacy : pharmacy
+        }),
+        contentType : 'application/json',
+
+        success : function(data) { 
+            console.log(data);  
+            var flag = false;
+            alert("Succesfully generated prescription!");
+            
+            var message1 = data;
+            
+            $.ajax({
+                type: "GET",
+                url: "/api/http/env",
+        
+                success : function(data2) {
+                    console.log(data2);
+        
+                    if(String(data2) === "Development"){
+                        sendPrescSftp(data);
+                        //message1 = data;
+                        console.log(message1);
+                    } else {
+                        sendPrescHttp(data);
+                        //message1 = data;
+                        console.log(message1);
+                    }
+                }
+            })
+
+            $.ajax({
+                type:'POST',
+                crossDomain: true,
+                url: 'http://localhost:8080/notification/add',
+                contentType : 'application/json',
+
+                data : JSON.stringify({
+                    message : message1
+                })
+    
+            })
+            
+        }
+    });
+
+
+}
+
+function checkGRPC() {
+
+    var name = $("#medID").val();
+    var amount = $("#amount").val();
+
+    amount1 = parseInt(amount);
+
+    $.ajax({
+        type: 'POST',
+        url: '/api/medicine/availability',
+
+        data : JSON.stringify({
+            Name : name,
+            Amount : amount1
         }),
 
         contentType : 'application/json',
@@ -128,39 +210,152 @@ function getAvailability() {
 
            }
     });
+
 }
 
-function sendPrescription(){
-    var patient = $("#patientName").val();
+function checkHTTP() {
+
     var name = $("#medID").val();
     var amount = $("#amount").val();
-    var pharmacy = "Benu";
 
     $.ajax({
         type: 'POST',
-        url: 'api/prescription/generate',
+        crossDomain: true,
+        url: 'http://localhost:8080/med/availability',
 
         data : JSON.stringify({
-            Patient : patient, 
-            Medicine : name,
-            Amount : parseInt(amount),
-            Pharmacy : pharmacy
+            name : name,
+            amount : amount
         }),
+
         contentType : 'application/json',
 
-        success : function(data) { 
-            console.log(data);               
-            alert("Succesfully generated prescription!");
+        success : function(data) {                
+            alert(data);
+
+            var divi = $("#orderTable"); 
+            divi.empty();
+
+            var myhtml = "";            
+
+            myhtml += '<div class="visible" id="tableDiv"><div class="container"><div class="row"><div class="col-lg-1"></div>';
+            myhtml += '<div class="col-lg-10"><table class="table"><thead>';
+            myhtml += '<tr><th scope="col">Pharmacy</th><th scope="col">Medicine</th><th scope="col">Amount required</th><th scope="col">Available for order</th><th scope="col"></th> </tr>';
+            myhtml += '</thead><tbody><tr>';
+
+            myhtml += '<td>';
+            myhtml += 'Benu';   
+            myhtml += '</td>';
+
+            myhtml += '<td>';
+            myhtml += name;   
+            myhtml += '</td>';
+
+            myhtml += '<td>';
+            myhtml += amount;   
+            myhtml += '</td>';
+
+            myhtml += '<td>';
+            if(data){
+                myhtml += '<button disabled class="btn btn-success">Yes</button>';  
+                myhtml += '</td>';
+
+                myhtml += '<td>';
+                myhtml += '<button id = "orderButton" class="btn btn-warning">Send prescription</button>';
+                myhtml += '</td>';
+
+            } else {
+                myhtml += '<button disabled class="btn btn-danger">No</button>'; 
+                myhtml += '</td>';
+            }
             
-            $.ajax({
-                type:'GET',
-                crossDomain: true,
-                url: 'http://localhost:8080/sftp/download/' + String(data)
+            myhtml += '</tr></tbody></table></div></div></div></div>';
+
+            divi.append(myhtml);
+
+            $("#orderButton").click(function() {
+                sendPrescription();
             });
-        }
+
+           }
     });
+
+}
+
+function getSpecSftp(name){
+
+    var name = $("#medID").val();
+
+    $.ajax({
+        type: 'GET',
+        crossDomain: true,
+        url: 'http://localhost:8080/medspec/' + name,
+        
+        success: function() {
+            $.ajax({
+                type: 'GET',
+                url: 'api/report/ftp/' + name,
+                
+                success: function() {
+                    getSpec();
+                }
+            })
+        }
+    })
 }
 
 
+function getSpecHttp(name){
+
+    $.ajax({
+        type: 'POST',
+        crossDomain: true,
+        url: 'http://localhost:8080/medspec/uploadhttp',
+
+        contentType : 'text/plain',
+
+        data : name,
+        
+        success : function() {
+             getSpec();
+        }
+    })
+}
+
+function sendPrescSftp(data) {
+
+    var message1;
+
+    $.ajax({
+        type:'GET',
+        crossDomain: true,
+        url: 'http://localhost:8080/sftp/download/' + String(data),
+        
+        success : function(data1) {
+             message1 = String(data1);
+             console.log(message1);
+        }
+
+    });
+    return message1;
+}
+
+function sendPrescHttp(data) {
+
+    var message1;
+
+    $.ajax({
+        type:'GET',
+        crossDomain: true,
+        url: 'api/http/httpfile/' + String(data),
+        
+        success : function(data1) {
+             message1 = String(data1);
+             console.log(message1);
+        }
+
+    });
+    return message1;
+}
 
 
