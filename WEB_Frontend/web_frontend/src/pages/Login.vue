8f13d855228700381c0fb49cea4e67f2d9da67e8
stojke13 <stojakovic_djordje@yahoo.com>
2020-12-25 12:39:25 +0100

    [WEB] feature: Initial frontend app vue

---
+++ WEB_Frontend/web_frontend/src/pages/Login.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+  <div class="col text-center">
+      <h2>Dobrodosli klinika Bla</h2>
+      <h4>Unesite vase podatke za prijavu:</h4>
+  </div>
+  <div class="row q-pa-md  justify-center">
+    <div style="width:300px;">
+    <q-form @submit="submit" class="q-gutter-md">
+        <q-input standout="bg-teal text-white" v-model="username" label="Korisnicko ime" :dense="dense" />
+        <q-input v-model="password" filled type="password" hint="Sifra" standout="bg-teal text-white" />
+        <div>
+        <q-btn label="Prijavi se" type="submit" color="teal"/>
+      </div>
+    </q-form>
+    </div>
+  </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data: function () {
+    return {
+      dense: false,
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    submit () {
+      alert('klik')
+    }
+  }
+}
+</script>
