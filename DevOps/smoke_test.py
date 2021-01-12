eb3adecf2eb2721015f9608394a55441fdad30e7
zukovicfilip <zukovicfilip@gmail.com>
2021-01-12 17:43:46 +0100

    Updated smoke test and docker-compose

--- DevOps/smoke_test.py
+++ DevOps/smoke_test.py
@@ -3,17 +3,21 @@ import requests
 import requests.exceptions
 
 def main():
-    URL = 'localhost:44395'
+    URL = 'http://localhost:44340/users/1'
     TIMEOUT = 10
 
     try:
         response = requests.get(URL, timeout = TIMEOUT)
+        print("Request sent")
     except:
+        print("Request not sent")
         sys.exit(-1)
 
     try:
         response.raise_for_status()
+        print("Successful")
     except HttpError:
+        print("Error")
         sys.exit(-1)
 
 if __name__ == "__main__":
